920de039e89712a0dc08a17d46e3d25ce65a6917
Takayuki TAKEMURA <takayuki.takemura@asoview.co.jp>
2025-04-22 05:00:28 +0900

    zshの設定をchezmoiで管理するようにした

---
+++ dot_config/zsh/conf.d/20-functions.zsh
@@ -0,0 +1,19 @@
+fbr() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+ghq-fzf() {
+  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+
+zle -N fbr
+zle -N ghq-fzf
---
+++ dot_config/zsh/conf.d/40-aliases.zsh
@@ -0,0 +1,3 @@
+alias ls='eza --icons --hyperlink'
+alias ll='ls -l'
+alias la='ll -a'
